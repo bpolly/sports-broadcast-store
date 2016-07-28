4b9626b33eb61bb6cc6d64f8db25a8026f246708
Braden Polly <bpolly6@gmail.com>
2016-07-28 14:02:05 -0400

    Added scrape route and tested rendering JSON response

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,15 @@
 class GamesController < ApplicationController
   def index
     @games = Game.all
-    response = HTTParty.get('https://www.stattleship.com/baseball/mlb/games',
+  end
+
+  def do_scraping
+    response = HTTParty.get('https://www.stattleship.com/baseball/mlb/games?team_id=mlb-cle&status=upcoming',
       headers: {
         "Content-Type" => 'application/json',
         "Authorization" => "Token token=#{ENV["STATTLESHIP_ACCESS_TOKEN"]}",
-        "Accept" => "application/vnd.stattleship.com; version=1",
-        "team_id" => "mlb-kc"})
+        "Accept" => "application/vnd.stattleship.com; version=1"})
     puts response
+    render json: response["games"].reverse, status: :ok
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   root 'games#index'
-
+  get '/scrape', to: 'games#do_scraping'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
