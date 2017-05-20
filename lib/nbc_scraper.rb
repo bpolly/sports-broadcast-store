808cf7d9a85a6a4f5b56158654b3290726b67477
Braden Polly <bpolly@covermymeds.com>
2017-05-20 12:15:30 -0400

    I don't like the MLB schedule tables

--- lib/nbc_scraper.rb
+++ lib/nbc_scraper.rb
@@ -147,6 +147,90 @@ class NbcScraper
   end # def scrape_nba
 
 
+  def scrape_mlb
+    schedule_released_month = 8
+    season_end_month = 6
+    season_months = ['04', '05', '06', '07', '08', '09', '10']
+
+    Team.mlb.each do |team|
+      starting_month = season_months.index(sprintf '%02i', Date.today.month)
+
+      # if current month is not in season,
+      if starting_month == nil
+        starting_month = 0
+      end
+
+      season_months[starting_month..-1].each do |month|
+        team_schedule = "http://scores.nbcsports.msnbc.com/mlb/teamstats.asp?teamno=#{team.nbc_team_id}&type=schedule&year=#{Time.now.year}&month=#{month}"
+        month = month.to_i
+        doc = Nokogiri::HTML(open(team_schedule))
+
+        current_year = Date.today.year
+
+        # Double check to make sure the calendar is on the right month
+        selected_tab_month_num = Date::ABBR_MONTHNAMES.index(doc.css(".shsTeamSchedTab strong").inner_text.gsub(/[^0-9A-Za-z]/, ''))
+        next if(selected_tab_month_num != month)
+
+
+        dates = doc.css('div#shsMLBTeamSched>table>tr.shsRow1Row, div#shsMLBTeamSched>table>tr.shsRow0Row').each do |row|
+          cols = row.css("td")
+          byebug if row.attributes['class'] == nil
+          if(cols.count > 2 && (row.attributes['class'].value != 'shsTableTtlRow') && (row.attributes['class'].value != 'shsColTtlRow'))
+            date_extractor = /[\w]+. ([\d]+)/
+            opponent_extractor = /(vs.|@|at) (.+)/
+            time_extractor = /([\d]+:[\d]+ (?:AM|PM) ET)/
+            day = date_extractor.match(cols[0].text)[1].to_i
+            match_sign = opponent_extractor.match(cols[1].text)? opponent_extractor.match(cols[1].text)[1] : byebug
+            opposing_team_text = opponent_extractor.match(cols[1].text)[2]
+            time = time_extractor.match(cols[2].text) ? time_extractor.match(cols[2].text)[1] : nil
+            next unless time
+            tv_networks = cols[3].text ? cols[3].text.gsub(/[[:space:]]/, '').split('/').join(',') : ''
+
+            home_game = match_sign == 'vs.'
+
+
+            extra_nickname = ''
+            if(opposing_team_text.downcase == 'los angeles')
+              team_no_extractor = /teamno=(\d+)/
+              team_no = team_no_extractor.match(cols[1].css('a').first.attributes['href'].value)[1]
+              extra_nickname = team_no == '12' ? 'clippers' : 'lakers'
+            end
+
+            team_no_extractor = /teamno=(\d+)/
+            team_no = team_no_extractor.match(cols[1].css('a').first.attributes['href'].value)[1]
+
+            opposing_team = Team.where(league: 'mlb').find_by(nbc_team_id: team_no)
+
+            #opposing_team = Team.find_given_nickname_and_league((extra_nickname != '' ? extra_nickname : opposing_team_text.downcase), "nba").first
+            byebug unless opposing_team
+
+            home_team_id = 0
+            away_team_id = 0
+            if(home_game)
+              home_team_id = team.id
+              away_team_id = opposing_team.id
+            else
+              home_team_id = opposing_team.id
+              away_team_id = team.id
+            end
+            if (home_team_id == 42 && month == 5)
+              byebug
+            end
+
+            date_time = Time.parse(time).to_datetime
+            date_time = date_time.change(day: day, month: month, year: current_year)
+
+            if(date_time > (Time.now - 1.day))
+              Game.find_or_create_by(home_team_id: home_team_id, away_team_id: away_team_id, date: date_time, tv_networks: tv_networks, league: 'mlb')
+            end
+            #game.save
+          end # if cols.count
+        end # dates = doc.css(".shsTable tr").each do |row|
+      end # SEASON_MONTHS.each do |month|
+    end # Team.nba.each do |team|
+  end # def scrape_nba
+
+
   def calculate_year(game_month, league)
     current_year = Time.now.year
     current_month = Time.now.month
