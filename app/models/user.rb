d0edba01848a0340c1a371d50e50e0776534cc7e
Braden Polly <bpolly@covermymeds.com>
2018-08-04 09:48:31 -0400

    Delete notification preferences when deleting a favorite team

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   validates_presence_of :email, :password_digest
   validates :email, uniqueness: true
-  has_many :user_notification_preferences
-  has_many :user_favorite_teams
+  has_many :user_notification_preferences, dependent: :destroy
+  has_many :user_favorite_teams, dependent: :destroy
   has_many :teams, through: :user_favorite_teams
+  alias_attribute :notification_preferences, :user_notification_preferences
   has_secure_password
 end
