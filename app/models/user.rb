2ec90caee12a9bd82cd2cf7b07a8b6736b36984b
Braden Polly <bpolly@covermymeds.com>
2018-09-28 15:18:14 -0400

    Cleanup user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ApplicationRecord
+  has_secure_password
   validates_presence_of :password_digest
   has_many :user_notification_preferences, dependent: :destroy
   has_many :user_favorite_teams, dependent: :destroy
   has_many :teams, through: :user_favorite_teams
   has_one :user_phone, dependent: :destroy
-  alias_attribute :phone, :user_phone
   has_one :user_email, dependent: :destroy
+
+  alias_attribute :phone, :user_phone
   alias_attribute :email, :user_email
   alias_attribute :notification_preferences, :user_notification_preferences
-  has_secure_password
 
   accepts_nested_attributes_for :user_email
 end
