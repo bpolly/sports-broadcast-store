d9642c0b82d5dacc36e496fcb75bca233b6db8f3
Braden Polly <bpolly@covermymeds.com>
2018-10-23 16:32:18 -0400

    Use Bulma sidebar

--- client/src/components/admin/AdminLayout.jsx
+++ client/src/components/admin/AdminLayout.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import '../../styles/admin/dashboard.scss';
+import '../../styles/admin/layout.scss';
 import AdminSidebar from './AdminSidebar';
 import axios from 'axios';
 import moment from 'moment-timezone';
--- client/src/components/admin/AdminSidebar.jsx
+++ client/src/components/admin/AdminSidebar.jsx
@@ -1,13 +1,37 @@
-import React, { Component } from 'react';
-import '../../styles/admin/sidebar.scss';
-
+import React, {Component} from 'react'
+import '../../styles/admin/sidebar.scss'
+import { NavLink } from 'react-router-dom'
 
 class AdminSidebar extends Component {
   render() {
-    return(
-      <div>
-        Sidebar here
-      </div>
+    return (
+      <aside className="menu">
+        <p className="menu-label">
+          General
+        </p>
+        <ul className="menu-list">
+          <li>
+            <NavLink exact to="/admin" activeClassName="is-active">Dashboard</NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/users" activeClassName="is-active">Users</NavLink>
+          </li>
+        </ul>
+        <p className="menu-label">
+          Notifications
+        </p>
+        <ul className="menu-list">
+          <li>
+            <NavLink to="/admin/payments" activeClassName="is-active">User Notifications</NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/transfers" activeClassName="is-active">Outgoing Notifications</NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/balance" activeClassName="is-active">Balance</NavLink>
+          </li>
+        </ul>
+      </aside>
     )
   }
 }
