9e74423abd1583d08ac1bfc8f79a8bd5c6b45846
Braden Polly <bpolly@covermymeds.com>
2018-11-02 13:59:45 -0400

    Remove unused imports

--- client/src/components/admin/AdminDashboard.jsx
+++ client/src/components/admin/AdminDashboard.jsx
@@ -1,8 +1,5 @@
 import React, { Component } from 'react'
 import '../../styles/admin/dashboard.scss'
-import AdminSidebar from './AdminSidebar'
-import axios from 'axios'
-import moment from 'moment-timezone'
 
 class AdminDashboard extends Component {
   render() {
