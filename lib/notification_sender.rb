3bef51e1e0b7e9726727301b1001e80a4517ec30
Braden Polly <bpolly@covermymeds.com>
2019-01-04 16:04:36 -0500

    Cleanup NotificationSender and rake task

--- lib/notification_sender.rb
+++ lib/notification_sender.rb
@@ -1,35 +1,53 @@
-module NotificationSender
-  class << self
-    def send_sms_notifications
-      UserNotificationPreference.where(team: NotificationFinder.teams_playing_in(minutes: 10)).sms.each do |preference|
-        TwilioClient.send_sms(to: preference.phone, body: sms_body(preference))
-      end
+class NotificationSender
+  attr_reader :minutes
+
+  def initialize(minutes = 10)
+    @minutes = minutes.to_i
+  end
+
+  def self.send_sms_notifications(minutes)
+    new(minutes).send_sms_notifications
+  end
+
+  def send_sms_notifications
+    sms_twilio_ids = []
+    preferences_for_teams_playing.each do |preference|
+      sms_twilio_ids << TwilioClient.send_sms(to: preference.user_phone.number, body: sms_body(preference))
     end
+    puts sms_twilio_ids.length
+  end
 
-    def sms_body(preference)
-      game = NotificationFinder.games_in(minutes: 10).with_team(preference.team).first
-      home_team = game.home_team
-      away_team = game.away_team
+  def preferences_for_teams_playing
+    UserNotificationPreference
+      .where(team: NotificationFinder.teams_playing_in(minutes: minutes))
+      .sms
+  end
 
-      <<~HEREDOC
-        #{emoji_by_league(game.league)}  #{away_team.name.titleize} @ #{home_team.name.titleize}
-        🕔  #{game.date.in_time_zone("Eastern Time (US & Canada)").strftime("%-l:%M%P")} ET
-        📺  #{game.tv_networks.split(",").map(&:strip).map(&:upcase).to_sentence}
+  private
 
-        Visit sportcasts.io to configure your team notifications.
-        Reply STOP to stop receiving these alerts.
-      HEREDOC
-    end
+  def sms_body(preference)
+    game = NotificationFinder.games_in(minutes: minutes).with_team(preference.team).first
+    home_team = game.home_team
+    away_team = game.away_team
 
-    def emoji_by_league(league)
-      case(league)
-      when 'nfl'
-        '🏈'
-      when 'nba'
-        '🏀'
-      when 'mlb'
-        '⚾'
-      end
+    <<~HEREDOC
+      #{emoji_by_league(game.league)}  #{away_team.name.titleize} @ #{home_team.name.titleize}
+      🕔  #{game.date.in_time_zone("Eastern Time (US & Canada)").strftime("%-l:%M%P")} ET
+      📺  #{game.tv_networks.split(",").map(&:strip).map(&:upcase).to_sentence}
+
+      Visit sportcasts.io to configure your team notifications.
+      Reply STOP to stop receiving these alerts.
+    HEREDOC
+  end
+
+  def emoji_by_league(league)
+    case(league)
+    when 'nfl'
+      '🏈'
+    when 'nba'
+      '🏀'
+    when 'mlb'
+      '⚾'
     end
   end
 end
--- lib/tasks/notifications.rake
+++ lib/tasks/notifications.rake
@@ -1,4 +1,4 @@
 desc "Looks for games starting in the next 10 minutes"
-task send_sms_notifications: [:environment] do
-  NotificationSender.send_sms_notifications
+task :send_sms_notifications, [:minutes] => [:environment] do |t, args|
+  NotificationSender.send_sms_notifications(args[:minutes])
 end
