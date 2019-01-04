8ea5aa180eb163dd1da61eed53614e075b70ef3d
Braden Polly <bpolly@covermymeds.com>
2019-01-04 16:34:33 -0500

    Rails 5 broke my keys

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,11 +2,11 @@ class JsonWebToken
   def self.encode(payload)
     # set token expiration time
     payload[:exp] = 1.day.from_now.to_i
-    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    JWT.encode(payload, Rails.application.secret_key_base)
   end
 
   def self.decode(token)
-    body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+    body = JWT.decode(token, Rails.application.secret_key_base)[0]
     HashWithIndifferentAccess.new body
 
   # raise custom error to be handled by custom handler
